3ebfddb71979d9f772ceae1bc2486004b845cd71
nandazzq <fernandatomazeto@gmail.com>
2025-12-17 22:24:04 -0300

    aada

---
+++ formulario-cadastro/script.js
@@ -0,0 +1,31 @@
+const formLogin = document.getElementById('formCadastro')
+
+console.log('formCadastro', formCadastro)
+
+formLogin,addEventListener('submit',(event) =>{
+    event.preventDefault()
+
+    // validação do formulário
+    // '. username > 6 caractere
+    // password > 8 caractere
+    //-> capturar as informações oelos ids dos inputs
+    //-> realizar as validações
+
+    const usernameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+
+if(usernameInput.value.length >= 6){
+    console.log("Validação bem sucedida");       
+} else{
+    console.log("Erro, o Username possui menos de 6 caracteres.");
+}
+
+if(passwordInput.value.length >= 8){
+    console.log("Validação em sucedida");
+} else{
+    console.log("Erro, a senha possui menos de 8 caracteres")
+}
+
+})
